aea450bccc93f66c149c82f3fa2a93b365c1897e
Acacio-Silva <acaciosilva90@gmail.com>
2022-11-28 10:40:16 -0300

    Criando Relacionamento

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const connection = require('../database/database')
+const Category = require('../categories/Category')
 
 const Article = connection.define('articles',{
     title:{
@@ -13,5 +14,10 @@ const Article = connection.define('articles',{
         allowNull: false
     }
 })
+
+Category.hasMany(Article) //Uma categoria tem varios artigos, relacionamento 1:N - hasMany()
+Article.belongsTo(Category) //um artigo pertence a uma categoria, relacionamento 1:1 -> belongsTo()
+
+//Article.sync({force:true});
 
 module.exports = Article;
